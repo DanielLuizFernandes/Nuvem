822dcd8559b6609654522241668ea90aa955d1fb
Daniel Luiz Guimarães Fernandes <143844048+DanielLuizFernandes@users.noreply.github.com>
2025-12-11 10:36:36 -0300

    .

--- JavaScript/Curso/aula13/ambiente.js
+++ JavaScript/Curso/aula13/ambiente.js
@@ -1,6 +1,6 @@
 var c = 1
 do {
-    console.log('Tudo bem?')
+    console.log(`Passo ${c}`)
     c++
 } while (c <= 6)
 
@@ -8,7 +8,7 @@ do {
 /*
 var c = 1
 while (c <= 6){
-    console.log('Tudo bem?')
+    console.log(`Passo ${c}`)
     c++
 }
 */
