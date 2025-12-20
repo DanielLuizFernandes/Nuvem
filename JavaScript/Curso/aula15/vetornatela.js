3f9e6ea345a94b16b81ead075263324adb1ff94e
Daniel Luiz Guimarães Fernandes <143844048+DanielLuizFernandes@users.noreply.github.com>
2025-12-20 06:39:09 -0300

    Final do curso Javascript

---
+++ JavaScript/Curso/aula15/vetornatela.js
@@ -0,0 +1,20 @@
+let valores = [3, 67, 1, 9, 4, 8]
+
+console.log(valores)
+/*
+console.log(valores[0])
+console.log(valores[1])
+console.log(valores[2])
+console.log(valores[3])
+console.log(valores[4])
+console.log(valores[5])
+
+
+for (let pos = 0;pos < valores.length; pos++){
+    console.log(`A posição ${pos+1} tem o valor ${valores[pos]}`)
+}
+*/
+
+for(let pos in valores) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
